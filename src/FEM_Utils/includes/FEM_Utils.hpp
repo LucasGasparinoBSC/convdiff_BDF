ea53316fdf4c16d484651cde8953ea66874a8b9b
Lucas Gasparino <lucas.gasparino@bsc.es>
2026-02-13 18:01:32 +0100

    Starting FEM_Utils lib.

--- src/FEM_Utils/includes/FEM_Utils.hpp
+++ src/FEM_Utils/includes/FEM_Utils.hpp
@@ -3,10 +3,28 @@
 
 #pragma once
 
+template<typename ITYPE, typename RTYPE>
 class FEM_Utils
 {
     private:
+        ITYPE pOrder;
+        ITYPE nDims;
+        ITYPE nNodes;
+        ITYPE nGausPts;
+        RTYPE *xgp1D;
+        RTYPE *wgp1D;
+        RTYPE *xgp;
+        RTYPE *wgp;
+        RTYPE *Ngp;
+        RTYPE *dNgp;
+        RTYPE *Lagr_gp;
+        RTYPE *dLagr_gp;
+        void set_GP1D();
     public:
+        FEM_Utils();
+        FEM_Utils(ITYPE p, ITYPE nD);
+        ~FEM_Utils();
+        void computeGaussPoints();
 };
 
 #endif // FEM_UTILS_HPP
--- src/FEM_Utils/sources/FEM_Utils.cpp
+++ src/FEM_Utils/sources/FEM_Utils.cpp
@@ -1,1 +1,34 @@
 #include "FEM_Utils.hpp"
+
+template<typename ITYPE, typename RTYPE>
+FEM_Utils<ITYPE, RTYPE>::FEM_Utils() {
+    pOrder = 0;
+    nDims = 0;
+    nNodes = 0;
+    nGausPts = 0;
+    xgp1D = nullptr;
+    wgp1D = nullptr;
+    xgp = nullptr;
+    wgp = nullptr;
+    Ngp = nullptr;
+    dNgp = nullptr;
+    Lagr_gp = nullptr;
+    dLagr_gp = nullptr;
+}
+
+template <typename ITYPE, typename RTYPE>
+FEM_Utils<ITYPE, RTYPE>::FEM_Utils(ITYPE p, ITYPE nD)
+{
+    pOrder = p;
+    nDims = nD;
+    nNodes = 0;
+    nGausPts = 0;
+    xgp1D = nullptr;
+    wgp1D = nullptr;
+    xgp = nullptr;
+    wgp = nullptr;
+    Ngp = nullptr;
+    dNgp = nullptr;
+    Lagr_gp = nullptr;
+    dLagr_gp = nullptr;
+}
